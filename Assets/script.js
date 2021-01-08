e5e8b8c578e943a76f7c97f7fcbc1b038ec8709c
Mr. Llanes <llanesm@gmail.com>
2021-01-08 00:31:22 -0600

    Weather icons showing, 5-day divs created, working on API.

--- Assets/script.js
+++ Assets/script.js
@@ -2,10 +2,11 @@
 $(document).ready(function() {
     console.log("document ready!");
 
+//Global Variables
 let APIKey = "21152b77f6ec2e79fa6cbfafe69cae6b";
 const citiesSearched = ['Houston', 'San Antonio'];
 
-
+// Function to create the "Searched" cities and saving them in an active button list
 function createCityBtns() {
     citiesSearched.forEach(function (city) {
         const cityBtn = $("<li>").addClass("list-group-item list-group-item-action").attr("data-type", city).text(city).attr("type", "button");
@@ -15,14 +16,23 @@ function createCityBtns() {
 
 createCityBtns();
 
-$('[name="add-city-button"]').on("click", function(event) {
+// Input field to search for city
+$('[name="add-city-button"]').on('click', function(event) {
     event.preventDefault();
-    let searchTerm = $('[name="add-city"]').val();
-    console.log("TEST EVENT LISTENER");
-    citiesSearched.push(searchTerm);
-    $("#cities-viewed").empty();
+    let cityName = $('[name="add-city"]').val();
+    console.log('TEST EVENT LISTENER');
+    citiesSearched.push(cityName);
+    $('#cities-viewed').empty();
     createCityBtns();
-    getCurrentWeather(searchTerm);
+    getCurrentWeather(cityName);
+    $('#search-input').val("");
+});
+
+// Giving functionality to the "list buttons" 
+$(document).on('click', 'li', function() {
+    let newBtn = $(this).text();
+    console.log(newBtn);
+    getCurrentWeather(newBtn);
 });
 
 $('body').on('click', '.city', function () {
@@ -30,7 +40,7 @@ $('body').on('click', '.city', function () {
 });
 
 
-// AJAX call to the OpenWeatherMap API
+// AJAX function/call to the OpenWeatherMap API
 function getCurrentWeather(searchTerm) {
 
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + APIKey + "&units=imperial";
@@ -51,8 +61,12 @@ function getCurrentWeather(searchTerm) {
         const unixTime = response.dt;
         const date = new Date(unixTime*1000);
         console.log(date.toLocaleDateString("en-US"));
+        const iconInfo = response.weather[0].icon;
+        console.log(iconInfo);
+        const currentIcon = "http://openweathermap.org/img/wn/" + iconInfo +"@2x.png";
 
-        $("#currentCity").html("<h2>" + response.name + " (" + date + ") </h1>");
+        // Passing the given response information to the correct location in the HTML code
+        $("#currentCity").html("<h2>" + response.name + " (" + date + ") " + "<img src=" + currentIcon + "></h1>");
         $("#temp").text("Temperature: " + response.main.temp + " °F  " + "(Feels like: " + response.main.feels_like + " °F)");
         $("#humidity").text("Humidity: " + response.main.humidity + " %");
         $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
@@ -62,6 +76,7 @@ function getCurrentWeather(searchTerm) {
     });
 }
 
+// AJAX function/call to get the UV Index from the OpenWeather API
 function getUVIndex(lat, lon) {
     let UVURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
@@ -84,6 +99,7 @@ function getUVIndex(lat, lon) {
     });
 }
 
+// AJAX function/call to get the 5-Day Forcast from the OpenWeather API
 function get5Day(searchTerm) {
     let fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=" + APIKey;
     console.log("Below should be the 5-Day Info");
